d82cfb80d590986a995ecc91a39f453eb9ca82e6
gahee99 <u1879@sookmyung.ac.kr>
2024-03-23 22:48:48 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/eodya/api/EodyaApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.eodya.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EodyaApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EodyaApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eodya/api/EodyaApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eodya.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EodyaApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
