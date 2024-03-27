628edd4773383cd28dd223425049b67ed96a6006
gahee99 <u1879@sookmyung.ac.kr>
2024-03-27 10:31:26 +0900

    test: 도메인엔티티 생성 테스트 코드 작성

---
+++ src/test/java/com/eodya/api/domain/AddressDepthTest.java
@@ -0,0 +1,46 @@
+package com.eodya.api.domain;
+
+import com.eodya.api.entity.AddressDepth1;
+import com.eodya.api.entity.AddressDepth2;
+import com.eodya.api.fixture.domain.AddressDepthFixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+public class AddressDepthTest {
+
+    @Test
+    @DisplayName("정상적으로 '시'를 생성할 수 있다.")
+    void createAddressDepth1_Success() {
+        //given
+        String city = "서울";
+        AddressDepth1 addressDepth1 = AddressDepthFixture.addressDepth1Build(city);
+
+        //when & then
+        assertEquals(addressDepth1.getName(), city);
+        assertEquals(addressDepth1.getDepth2().size(), 0);
+    }
+
+    @Test
+    @DisplayName("정상적으로 '구'를 생성할 수 있다.")
+    void createAddressDepth2_Success() {
+        //given
+        String city = "서울";
+        AddressDepth1 addressDepth1 = AddressDepthFixture.addressDepth1Build(city);
+
+        String gu = "강남구";
+        AddressDepth2 addressDepth2 = AddressDepthFixture.addressDepth2Build(gu, addressDepth1);
+
+        // when & then
+        assertNotNull(addressDepth2.getDepth1());
+        assertEquals(addressDepth2.getName(), gu);
+
+        // 연관관계 확인
+        assertEquals(addressDepth1.getName(), city);
+        assertEquals(addressDepth1.getDepth2().size(), 1);
+        assertEquals(addressDepth1.getDepth2().get(0).getName(), gu);
+    }
+}
---
+++ src/test/java/com/eodya/api/domain/UserTest.java
@@ -0,0 +1,42 @@
+package com.eodya.api.domain;
+
+import com.eodya.api.entity.User;
+import com.eodya.api.fixture.domain.UserFixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class UserTest {
+
+    @Test
+    @DisplayName("정상적으로 유저를 생성할 수 있다.")
+    void createUser_Success() {
+        //given
+        String userName = "재훈";
+        String OAuthId = "123456";
+        String OAuthProvider = "KAKAO";
+        User user = UserFixture.userBuild(userName, OAuthId, OAuthProvider);
+
+        //when & then
+        assertNotNull(user.getNickname());
+        assertEquals(user.getNickname(), userName);
+    }
+
+    @Test
+    @DisplayName("새로운 유저를 생성하면, 유저의 북마크, 추천, 리뷰의 수는 0이다.")
+    void createUser_bookmark_review_recommend_size_test() {
+        //given
+        Long fakeId = 1L;
+        String userName = "재훈";
+        String OAuthId = "123456";
+        String OAuthProvider = "KAKAO";
+        User user = UserFixture.userBuild(userName, OAuthId, OAuthProvider);
+
+        //when & then
+        assertEquals(user.getBookmarks().size(), 0);
+        assertEquals(user.getRecommendations().size(), 0);
+        assertEquals(user.getRecommendations().size(), 0);
+    }
+}
