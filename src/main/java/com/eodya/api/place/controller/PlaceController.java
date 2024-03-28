90012fd2eb2680bb5c3569588189fdeba8202d4e
gahee99 <u1879@sookmyung.ac.kr>
2024-03-29 01:23:53 +0900

    feat: 장소 등록 api개발

---
+++ src/main/java/com/eodya/api/place/controller/PlaceController.java
@@ -0,0 +1,33 @@
+package com.eodya.api.place.controller;
+
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+
+import com.eodya.api.place.dto.request.PlaceCreateRequest;
+import com.eodya.api.place.service.PlaceService;
+import com.eodya.api.users.config.Login;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/place")
+public class PlaceController {
+
+    private final PlaceService placeService;
+
+    @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createPlaceAndReview(@Login Long userId, @Valid @ModelAttribute PlaceCreateRequest request) {
+
+        placeService.createPlaceAndReview(userId, request);
+
+        return ResponseEntity.status(NO_CONTENT)
+                .build();
+    }
+}
---
+++ src/main/java/com/eodya/api/place/repository/AddressDepth2Repository.java
@@ -0,0 +1,8 @@
+package com.eodya.api.place.repository;
+
+import com.eodya.api.address.domain.AddressDepth2;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressDepth2Repository extends JpaRepository<AddressDepth2, Long> {
+
+}
