ec2ab8293f9b6107824be4600b1049be7f26a890
gahee99 <u1879@sookmyung.ac.kr>
2024-03-28 15:05:41 +0900

    feat: JWT Token manager 생성

---
+++ src/main/java/com/eodya/api/users/config/AuthConfig.java
@@ -0,0 +1,12 @@
+package com.eodya.api.users.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+public class AuthConfig {
+    @Value("${jwt.secret}")
+    private String jwtSecretKey;
+}
