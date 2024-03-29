c1b4d74d01a9bba91d23691133c3be615c05090a
jay-so <merry9504@naver.com>
2024-03-30 03:20:50 +0900

    refactor: 장소 엔티티, fixture 리팩토링

--- src/test/java/com/eodya/api/domain/PlaceTest.java
+++ src/test/java/com/eodya/api/domain/PlaceTest.java
@@ -10,19 +10,40 @@ import com.eodya.api.fixture.domain.PlaceFixture;
 import com.eodya.api.fixture.domain.UserFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.locationtech.jts.geom.Point;
+
+import java.util.List;
 
 import static com.eodya.api.users.domain.OAuthProvider.KAKAO;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class PlaceTest {
+class PlaceTest {
 
-    String placeName = "테스트 스팟 생성";
-    String image = "이미지";
-    OAuthProvider OAuthProvider = KAKAO;
-    User user = UserFixture.userBuild("가희", "1234", OAuthProvider);
+    @Test
+    @DisplayName("정상적으로 장소를 생성할 수 있다.")
+    void createPlace_Success() {
+        // given
+        Place testPlace = PlaceFixture.placeBuild();
 
 
+        // when & then
+        assertNotNull(testPlace.getUser());
+        assertNotNull(testPlace.getPoint());
+        assertNotNull(testPlace.getPoint());
+        assertNotNull(testPlace.getAddressDetail());
+    }
 
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    @DisplayName("여러 개의 장소를 성공적으로 생성할 수 있다.")
+    void createPlaces_Success(int count) {
+        // given
+        List<Place> testPlaces = PlaceFixture.placesBuild(count);
+
+        // when & then
+        assertEquals(count, testPlaces.size());
+    }
 }
--- src/test/java/com/eodya/api/fixture/domain/PlaceFixture.java
+++ src/test/java/com/eodya/api/fixture/domain/PlaceFixture.java
@@ -8,17 +8,22 @@ import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class PlaceFixture {
 
-    private static GeometryFactory geometryFactory = new GeometryFactory();
-    public static Place placeBuild(
-            Point point,
-            String name,
-            String addressDetail,
-            User user,
-            AddressDepth1 addressDepth1,
-            AddressDepth2 addressDepth2
-    ) {
+    private static final GeometryFactory geometryFactory = new GeometryFactory();
+
+    public static Place placeBuild() {
+        Point point = pointBuild(37.5665, 126.9780);
+        String name = "테스트 스팟 장소";
+        String addressDetail = "서울특별시 강남구 논현동";
+        User user = UserFixture.userBuild();
+        AddressDepth1 addressDepth1 = AddressDepthFixture.addressDepth1Build();
+        AddressDepth2 addressDepth2 = AddressDepthFixture.addressDepth2Build();
+
         return Place.builder()
                 .point(point)
                 .name(name)
@@ -29,9 +34,18 @@ public class PlaceFixture {
                 .build();
     }
 
-    public static Point pointBuild(double x, double y) {
-        Coordinate coord = new Coordinate(x, y);
-        return geometryFactory.createPoint(coord);
+    public static List<Place> placesBuild(int count) {
+        List<Place> places = new ArrayList<>();
 
+        IntStream.range(0, count).forEach(i -> {
+            places.add(placeBuild());
+        });
+
+        return places;
+    }
+
+    public static Point pointBuild(double x, double y) {
+        Coordinate coordinate = new Coordinate(x, y);
+        return geometryFactory.createPoint(coordinate);
     }
 }
