bb2164cf7cdb844647ef0a2af8439844476aa3f7
jay-so <merry9504@naver.com>
2024-04-11 19:40:17 +0900

    refactor: 유저 엔티티 테스트 리팩토링

--- src/test/java/com/eodya/api/fixture/domain/UserFixture.java
+++ src/test/java/com/eodya/api/fixture/domain/UserFixture.java
@@ -3,28 +3,27 @@ package com.eodya.api.fixture.domain;
 import com.eodya.api.users.domain.OAuthProvider;
 import com.eodya.api.users.domain.User;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class UserFixture {
 
-//    public static User userBuild() {
-//        return User.builder()
-//                .nickname("testNickName")
-//                .oauthId(1L)
-//                .oauthProvider(OAuthProvider.KAKAO)
-//                .build();
-//    }
-//
-//    public static List<User> usersBuild(int count, OAuthProvider OAuthProvider) {
-//        return IntStream.range(0, count)
-//                .mapToObj(i -> User.builder()
-//                        .nickname(String.format("testUser%d", i))
-//                        .oauthId(Long.valueOf("OAuthId%d", i))
-//                        .oauthProvider(OAuthProvider)
-//                        .build())
-//                .collect(Collectors.toList());
-//    }
+    public static User userBuild() {
+        return User.builder()
+                .nickname("테스트 유저 닉네임")
+                .oauthId(1L)
+                .oauthProvider(OAuthProvider.KAKAO)
+                .build();
+    }
+
+    public static List<User> usersBuild(int count, OAuthProvider OAuthProvider) {
+        return IntStream.range(0, count)
+                .mapToObj(i -> User.builder()
+                        .nickname(String.format("테스트 유저 닉네임 %d", i))
+                        .oauthId(i + 1L)
+                        .oauthProvider(OAuthProvider)
+                        .build())
+                .collect(Collectors.toList());
+    }
 }
